e1d51a2fcde473ece462ade7f0af97e04f9d7fbb
D.Sharath <dsharath217@gmail.com>
2026-07-23 05:13:26 +0000

    add payment to stage

--- models/raw/raw_orders.sql
+++ models/raw/raw_orders.sql
@@ -1,1 +1,10 @@
-select * from {{ source("raw_jaffle_shop", "orders") }}
+with renamed as (
+select id
+, user_id
+, order_date
+, status
+from {{ source("raw_jaffle_shop", "orders") }}
+)
+
+select *
+from renamed
--- models/raw/raw_payment.sql
+++ models/raw/raw_payment.sql
@@ -1,2 +1,12 @@
-select *
+with renamed as (
+select id
+, orderid
+, paymentmethod
+, amount
+, status
+, created
 from {{ source('raw_stripe', 'payment') }}
+)
+
+select *
+from renamed
